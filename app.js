b85482b72315c11695d4a43c48df6f2243cb8e8d
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-05 23:56:41 +0530

    making some progress

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+let screen = document.querySelector(".screen");
+
+
 function renderScren() {
-    let screen = document.querySelector(".screen");
-    for (let i = 1; i <= 6000; i++) {
-        let cell = document.createElement("div");
-        let name = "cell" + i;
-        cell.classList.add(name);
-        cell.classList.add("cell");
-        screen.appendChild(cell);
+
+    for (let i = 1; i <= 60; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let i = 1; i <= 100; i++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+
+        row.addEventListener("mouseover", changeColour);
+        row.addEventListener("mouseleave", changeColour);
+        screen.appendChild(row);
     }
 }
 
-// renderScren();
+function changeColour(event) {
+    if (event.target.classList.contains("cell")) {
+        event.target.backgroundColour = "black";
+        console.log("flag");
+    }
+}
+
+
+
+renderScren();
